419389cc453aab3ddddb8e3d77b7b941709e97f9
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-07 19:34:59 +0100

    updated vercel_app.py file

--- user_authentication/vercel_app.py
+++ user_authentication/vercel_app.py
@@ -1,3 +1,13 @@
-from user_authentication.wsgi import application
+import os
+import sys
 
-app = application
+from django.core.wsgi import get_wsgi_application
+
+# Add your project to the sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), "user_authentication"))
+
+# Set the settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "user_authentication.settings")
+
+# Get the WSGI application
+application = get_wsgi_application()
