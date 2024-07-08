08d4de4d3e1975d7e6ad5b20ab8b454b27663c4f
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-08 18:55:19 +0100

    updated the organization detail view endpoint

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, UserDetailView, OrganisationViewSet, AddUserToOrganisationView
+from .views import RegisterView, LoginView, UserDetailView, OrganisationViewSet, AddUserToOrganisationView, OrganisationDetailView
 
 
 router = DefaultRouter()
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/users/<uuid:id>', UserDetailView.as_view()),
     path('api/', include(router.urls)),
     # path('api/organisations', OrganisationListView.as_view()),
-    # path('api/organisations/<uuid:orgId>', OrganisationDetailView.as_view()),
+    path('api/organisations/<uuid:orgId>', OrganisationDetailView.as_view()),
     # path('api/organisations', CreateOrganisationView.as_view()),
     path('api/organisations/<uuid:orgId>/users', AddUserToOrganisationView.as_view()),
 ]
